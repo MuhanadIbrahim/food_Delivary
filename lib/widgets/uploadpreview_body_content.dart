ac15d09724ecc171a1707552671b73bdb77f3821
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-10-02 01:20:45 +0300

    finish create upload preview screan

--- lib/widgets/uploadpreview_body_content.dart
+++ lib/widgets/uploadpreview_body_content.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'custom_button.dart';
 
 class UploadPreviewBodyContent extends StatelessWidget {
   const UploadPreviewBodyContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Spacer(
+            flex: 2,
+          ),
+          SvgPicture.asset('assets/images/Icon Back.svg'),
+          const Spacer(
+            flex: 1,
+          ),
+          const Text(
+            'Upload Your Photo\nProfile',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          const Text(
+              'This data will be displayed in your account \nprofile for security'),
+          const Spacer(
+            flex: 1,
+          ),
+          Padding(
+            padding:const  EdgeInsets.symmetric(horizontal: 45.0),
+            child: Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/profilepic.jpg'),
+                ),
+              ),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset('assets/images/Close Icon.svg'),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(
+            flex: 8,
+          ),
+          GestureDetector(
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 100.0),
+              child: CustomButton(title: 'Next'),
+            ),
+          ),
+          const Spacer(
+            flex: 2,
+          )
+        ],
+      ),
+    );
   }
 }
