62c8d7e22b1d5a1f4f0ca5347a71a719f59123e9
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-10-02 23:52:29 +0300

    finish rest password notication success screan

--- lib/widgets/restpassword_succesNotication_body.dart
+++ lib/widgets/restpassword_succesNotication_body.dart
@@ -1,14 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:food_delivery_app/constans/constans.dart';
 import 'package:food_delivery_app/widgets/restpassword_successn_notication_body_content.dart';
-
 
 class RestpasswordSuccesNoticationBody extends StatelessWidget {
   const RestpasswordSuccesNoticationBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack(children: [SvgPicture.asset('assets/images/backGroundApp.svg'),const RestpasswordSuccesNoticationBodyContent()],),);
+    return Scaffold(
+      backgroundColor: kprimaryBackGroundColor,
+      body: Stack(
+        children: [
+          SvgPicture.asset('assets/images/backGroundApp.svg'),
+          const RestpasswordSuccesNoticationBodyContent()
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/restpassword_successn_notication_body_content.dart
+++ lib/widgets/restpassword_successn_notication_body_content.dart
@@ -1,11 +1,45 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'custom_button.dart';
 
 class RestpasswordSuccesNoticationBodyContent extends StatelessWidget {
   const RestpasswordSuccesNoticationBodyContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Spacer(
+            flex: 3,
+          ),
+          SvgPicture.asset('assets/images/Illustration.svg'),
+          const SizedBox(
+            height: 10,
+          ),
+          SvgPicture.asset('assets/images/congrate.svg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Password reset succesful',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+          ),
+          const Spacer(
+            flex: 3,
+          ),
+          GestureDetector(
+            child: const CustomButton(
+              title: 'Back',
+            ),
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 }
