d6665c487622abb2ce6bd8c9925229830e282bb9
Muhanned Ebrahim <parhoegypt@gmail.com>
2023-10-10 18:51:33 +0300

    add flutter firbase auth package and add auth stae change to home screan and conver it to statfullwaidget

--- lib/views/home_screan.dart
+++ lib/views/home_screan.dart
@@ -1,8 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/constans/constans.dart';
 import 'package:food_delivery_app/widgets/home_screan_body.dart';
 
-class HomeScrean extends StatelessWidget {
+class HomeScrean extends StatefulWidget {
   const HomeScrean({super.key});
+
+  @override
+  State<HomeScrean> createState() => _HomeScreanState();
+}
+
+class _HomeScreanState extends State<HomeScrean> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+        Navigator.pushReplacementNamed(context, kLoginScrean);
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
