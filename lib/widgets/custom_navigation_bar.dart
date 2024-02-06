b4d52d815a9eb6eb3e1c3bc16cba00e91a870243
Muhanned Ebrahim <parhoegypt@gmail.com>
2024-02-06 21:08:07 +0200

    dispaly the animation when press in custom navigation par

--- lib/widgets/custom_navigation_bar.dart
+++ lib/widgets/custom_navigation_bar.dart
@@ -1,14 +1,41 @@
-
 import 'package:flutter/material.dart';
 
 import 'borderless_box_decoration.dart';
 import 'gradient_icon.dart';
-import 'home_button_icon.dart';
 
-class CustomNavigationBar extends StatelessWidget {
-  const CustomNavigationBar({
-    super.key,
-  });
+class CustomNavigationBar extends StatefulWidget {
+  const CustomNavigationBar({super.key});
+
+  @override
+  State<CustomNavigationBar> createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _handleIconPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _animationController.forward(from: 0.0);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,33 +43,69 @@ class CustomNavigationBar extends StatelessWidget {
       decoration: borderLessBoxDecoration(),
       height: 65,
       width: double.infinity,
-      child: const Padding(
-        padding: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            HomeButtonIcon(),
+            _buildIcon(0, Icons.home, 'Home'),
+            _buildIcon(1, Icons.person, 'Profile'),
+            _buildIcon(2, Icons.shopping_cart, 'Cart'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIcon(int index, IconData icon, String text) {
+    return AnimatedContainer(
+      duration: _animationController.duration!,
+      curve: Curves.easeInOut,
+      // Limit width to prevent overflow
+      width: _selectedIndex == index
+          ? MediaQuery.of(context).size.width *
+              0.4 // Adjust based on your layout
+          : 40,
+      height: 44,
+      decoration: ShapeDecoration(
+        gradient: _selectedIndex == index
+            ? LinearGradient(
+                begin: Alignment(0.99, -0.15),
+                end: Alignment(-0.99, 0.15),
+                colors: [Color(0xFFecfcf2), Color(0xFFe8f9f1)],
+              )
+            : null,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: InkWell(
+        onTap: () => _handleIconPressed(index),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
             GradientIcon(
-                size: 40,
-                icon: Icons.person,
-                gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [Color(0xff9ae5bf), Color(0xffd7f6e6)])),
-            GradientIcon(
-                size: 40,
-                icon: Icons.shopping_cart,
-                gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [Color(0xff9ae5bf), Color(0xffd7f6e6)])),
-            // GradientIcon(
-            //     size: 40,
-            //     icon: Icons.wechat,
-            //     gradient: LinearGradient(
-            //         begin: Alignment.centerRight,
-            //         end: Alignment.centerLeft,
-            //         colors: [Color(0xff9ae5bf), Color(0xffd7f6e6)]))
+              size: _selectedIndex == index ? 37 : 40,
+              icon: icon,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xff48dd89), Color(0xff1cc37a)],
+              ),
+            ),
+            if (_selectedIndex == index)
+              Flexible(
+                // Wrap text to avoid overflow
+                child: Text(
+                  text,
+                  style: TextStyle(
+                    color: Color(0xFF09051C),
+                    fontSize: 15,
+                    fontFamily: 'BentonSans Medium',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
           ],
         ),
       ),
